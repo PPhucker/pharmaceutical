68a725c86886d1974df8d5d01ff837cb75f7d4a8
PPhucker <alexandr.priponov.pphucker@gmail.com>
2022-12-20 11:32:50 +0300

    Run command php artisan ui vue --auth

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,18 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js').
-    sass('resources/sass/app.scss', 'public/css').
-    postCss('resources/css/app.css', 'public/css/main.css').
-    setPublicPath('public');
-
-mix.webpackConfig({
-    stats: {
-        children: true,
-    },
-});
+    vue().
+    sass('resources/sass/app.scss', 'public/css');
