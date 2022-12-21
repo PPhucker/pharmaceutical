188e158254f6117fe2b3234e77ec22ff43df2834
PPhucker <alexandr.priponov.pphucker@gmail.com>
2022-12-21 14:49:04 +0300

    Create Middleware\Admin.php

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request                                       $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     *
+     * @return Response|RedirectResponse
+     */
+    final public function handle(Request $request, Closure $next)
+    {
+        if (auth()->user()->hasRole(['admin'])) {
+            return $next($request);
+        }
+
+        abort(404);
+    }
+}
