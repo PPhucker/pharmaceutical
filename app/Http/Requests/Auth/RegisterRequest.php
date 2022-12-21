3878caaf3c3e9f76e590e10310f097b6f95881ec
PPhucker <alexandr.priponov.pphucker@gmail.com>
2022-12-21 15:00:17 +0300

    Create form request Auth\RegisterRequest.php

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    final public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    final public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:60'
+            ],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'ends_with:' . env('CORPORATE_MAIL_DOMAIN'),
+                'max:60',
+                Rule::unique('users', 'email')->whereNull('deleted_at'),
+            ],
+
+            'password' => [
+                'required',
+                'string',
+                'min:8'
+            ],
+
+            'roles' => [
+                'required'
+            ],
+            'permissions' => [
+                'required'
+            ],
+        ];
+    }
+}
