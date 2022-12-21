fc30dfe5663eaaaff13a30b0094d659dd382780a
PPhucker <alexandr.priponov.pphucker@gmail.com>
2022-12-21 14:59:05 +0300

    Update route file web.php
    A group of routes has been added to the main route file, which is available only to authorized users who have confirmed their e-mail.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', static function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+    ->name('home');
+
+Route::middleware(['auth', 'verified'])->group(
+    static function () {
+        require_once __DIR__ . '/admin/web.php';
+    }
+);
