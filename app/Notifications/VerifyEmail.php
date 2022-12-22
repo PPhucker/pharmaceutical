c400e0e4c4faec966a8875acde50e5dafbb848b8
PPhucker <alexandr.priponov.pphucker@gmail.com>
2022-12-22 12:14:13 +0300

    Create notification class VerifyEmail.php

---
+++ app/Notifications/VerifyEmail.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Notifications;
+
+use Closure;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\URL;
+
+class VerifyEmail extends Notification
+{
+    /**
+     * The callback that should be used to create to verify email URL.
+     *
+     * @var Closure|null
+     */
+    public static $createUrlCallback;
+
+    /**
+     * The callback that should be used to build the mail message.
+     *
+     * @var Closure|null
+     */
+    public static $toMailCallback;
+
+    /**
+     * Get the notification's channels.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Build the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+        if (static::$toMailCallback) {
+            return call_user_func(static::$toMailCallback, $notifiable, $verificationUrl);
+        }
+
+        return $this->buildMailMessage($verificationUrl);
+    }
+
+    /**
+     * Get to verify email notification mail message for the given URL.
+     *
+     * @param string $url
+     *
+     * @return MailMessage
+     */
+    protected function buildMailMessage(string $url)
+    {
+        return (new MailMessage)
+            ->subject(__('Email Verification'))
+            ->line(__('Please click the button below to verify your email address.'))
+            ->action(__('Email Verification'), $url)
+            ->line(__('If you have not created an account, do not take any action.'));
+    }
+
+    /**
+     * Get the verification URL for the given notifiable.
+     *
+     * @param mixed $notifiable
+     *
+     * @return string
+     */
+    protected function verificationUrl($notifiable)
+    {
+        if (static::$createUrlCallback) {
+            return call_user_func(static::$createUrlCallback, $notifiable);
+        }
+
+        return URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
+        );
+    }
+
+    /**
+     * Set a callback that should be used when creating the email verification URL.
+     *
+     * @param Closure $callback
+     *
+     * @return void
+     */
+    public static function createUrlUsing(Closure $callback)
+    {
+        static::$createUrlCallback = $callback;
+    }
+
+    /**
+     * Set a callback that should be used when building the notification mail message.
+     *
+     * @param Closure $callback
+     *
+     * @return void
+     */
+    public static function toMailUsing(Closure $callback)
+    {
+        static::$toMailCallback = $callback;
+    }
+}
