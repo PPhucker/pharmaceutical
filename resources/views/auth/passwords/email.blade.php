135516a0046a3f34565a7202d5874e43a258f70b
PPhucker <alexandr.priponov.pphucker@gmail.com>
2022-12-22 09:49:18 +0300

    Update views email.blade.php and reset.blade.php

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,42 +1,49 @@
 @extends('layouts.app')
-
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Reset Password') }}</div>
-
+                    <div class="card-header bg-primary text-white bg-gradient">
+                        {{ __('Reset Password') }}
+                    </div>
                     <div class="card-body">
                         @if (session('status'))
-                            <div class="alert alert-success" role="alert">
+                            <div class="alert alert-success"
+                                 role="alert">
                                 {{ session('status') }}
                             </div>
                         @endif
-
-                        <form method="POST" action="{{ route('password.email') }}">
+                        <form method="POST"
+                              action="{{ route('password.email') }}">
                             @csrf
-
                             <div class="row mb-3">
                                 <label for="email"
-                                       class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
+                                       class="col-md-4 col-form-label text-md-end">
+                                    {{ __('E-Mail') }}
+                                </label>
                                 <div class="col-md-6">
-                                    <input id="email" type="email"
-                                           class="form-control @error('email') is-invalid @enderror" name="email"
-                                           value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                                    <input id="email"
+                                           type="email"
+                                           class="form-control
+                                           @error('email') is-invalid @enderror"
+                                           name="email"
+                                           value="{{ old('email') }}"
+                                           required>
                                     @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                                    <span class="invalid-feedback"
+                                          role="alert">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
                                     </span>
                                     @enderror
                                 </div>
                             </div>
-
                             <div class="row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit"
+                                            class="btn btn-primary">
                                         {{ __('Send Password Reset Link') }}
                                     </button>
                                 </div>
