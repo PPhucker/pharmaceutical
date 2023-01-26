ba7fe497bc68994292107b2958caedaee21003e5
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-01-26 12:38:45 +0300

    update routes\admin\users.php

--- routes/admin/users.php
+++ routes/admin/users.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users/register', [RegisterController::class, 'showRegistrationForm'])
     ->name('users.register');
+
+Route::resource('users', UserController::class)
+    ->except('show');
+
+Route::controller(UserController::class)->group(static function () {
+    Route::post('/users/{user}/restore', 'restore')
+        ->name('users.restore')
+        ->withTrashed();
+    Route::post('/users/{user}/force/delete', 'forceDelete')
+        ->name('users.force.delete')
+        ->withTrashed();
+    Route::get('/users/{role}', 'show')
+        ->name('users.show');
+});
