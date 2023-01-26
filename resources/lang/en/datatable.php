ad497e8ec073261f4b450b1d10f615933e88f73b
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-01-26 12:27:25 +0300

    create localization files

---
+++ resources/lang/en/datatable.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'entries' => [
+        'entries' => 'Entries',
+        'all' => 'All',
+        'of' => 'of',
+        'no' => 'No Entries',
+        'no_to_find' => 'No Entries To Find',
+        'show' => 'Show',
+        'working' => 'Working',
+        'trashed' => 'Trashed'
+    ],
+    'buttons' => [
+        'hide' => 'Hide',
+        'save_as' => 'Save As',
+        'delete' => 'Delete',
+        'edit' => 'Edit',
+        'restore' => 'Retore'
+    ],
+    'search' => 'Search',
+    'wait' => 'Wait'
+];
