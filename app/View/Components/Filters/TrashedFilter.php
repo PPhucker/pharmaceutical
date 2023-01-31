b18a718a0ed9b4bc3148c093484497eef51fa51b
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-01-31 12:40:03 +0300

    create components

---
+++ app/View/Components/Filters/TrashedFilter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\View\Components\Filters;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class TrashedFilter extends Component
+{
+    public $tableId;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($tableId)
+    {
+        $this->tableId = $tableId;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return View
+     */
+    public function render()
+    {
+        return view('components.filters.trashed-filter');
+    }
+}
---
+++ resources/views/components/filters/select-filter.blade.php
@@ -0,0 +1,14 @@
+<div class="list-inline-item pb-2">
+    <div class="input-group input-group-sm">
+        <span class="input-group-text rounded-start">
+            {{$title}}
+        </span>
+        <select name="{{$name}}"
+                class="form-control form-control-sm">
+            <option value="{{null}}">
+                {{__('datatable.entries.all')}}
+            </option>
+            {{$slot}}
+        </select>
+    </div>
+</div>
