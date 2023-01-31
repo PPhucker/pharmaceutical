5904fe07e8dfa4c282a2df0ce0d791b07ae965f9
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-01-31 12:41:52 +0300

    update localization

---
+++ resources/lang/en/logs.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'actions' => [
+        'action' => 'Action',
+        'create' => 'Creating',
+        'destroy' => 'Deleting',
+        'restore' => 'Restoring',
+        'update' => 'Updating',
+    ],
+
+    'after' => 'After',
+    'before' => 'Before',
+    'changes' => 'Changes',
+    'fail' => 'Failed to apply filter',
+    'item_id' => 'Item ID',
+    'log' => 'Log',
+    'logs' => 'Logs',
+    'model' => 'Model',
+    'table' => 'Table',
+    'time' => 'Time',
+];
