c185f9778fa62d036f7fcfceacf38674bc79655c
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-02-16 12:14:04 +0300

    update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ require('./bootstrap');
 
 global.$ = global.jQuery = require('jquery');
 
+require('suggestions-jquery');
+
 import {DataTable} from './components/data-table.js';
 
 global.DataTable = DataTable;
@@ -9,5 +11,9 @@ global.DataTable = DataTable;
 import {Filter} from './components/filter-table-records.js';
 
 global.Filter = Filter;
+
+import {DaData} from './components/dadata/DaData.js';
+
+global.DaData = DaData;
 
 
