40db4cfaab20c853dd7c9605dd20659d4877d43d
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-02-16 13:14:45 +0300

    create BankAccountDetails
    created Model, Repository, Controller, FormRequests, Observer and views

---
+++ app/Http/Controllers/Admin/Organizations/BankAccountDetailController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Organizations;
+
+use App\Http\Controllers\CoreController;
+use App\Http\Requests\Admin\Organizations\BankAccountDetails\StoreBankAccountDetailRequest;
+use App\Http\Requests\Admin\Organizations\BankAccountDetails\UpdateBankAccountDetailRequest;
+use App\Models\Admin\Organizations\BankAccountDetail;
+use App\Repositories\Admin\Organizations\BankAccountDetailRepository;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+
+class BankAccountDetailController extends CoreController
+{
+    protected function getRepository()
+    {
+        return BankAccountDetailRepository::class;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreBankAccountDetailRequest $request
+     *
+     * @return RedirectResponse
+     */
+    public function store(StoreBankAccountDetailRequest $request)
+    {
+        $validated = $request->validated()['bank_account_detail'];
+
+        $bankAccountDetail = BankAccountDetail::create(
+            [
+                'organization_id' => (int)$validated['organization_id'],
+                'user_id' => Auth::user()->id,
+                'bank' => $validated['bank'],
+                'payment_account' => $validated['payment_account'],
+            ]
+        );
+
+        return back()
+            ->with(
+                'success',
+                __(
+                    'contractors.bank_account_details.actions.create.success',
+                    ['name' => $bankAccountDetail->payment_account]
+                )
+            );
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UpdateBankAccountDetailRequest $request
+     *
+     * @return RedirectResponse
+     */
+    public function update(UpdateBankAccountDetailRequest $request)
+    {
+        $validated = $request->validated();
+
+        foreach ($validated['bank_account_details'] as $item) {
+            $bankAccountDetail = BankAccountDetail::find((int)$item['id']);
+
+            $bankAccountDetail->fill(
+                [
+                    'user_id' => Auth::user()->id,
+                    'bank' => $item['bank'],
+                    'payment_account' => $item['payment_account'],
+                ]
+            )
+                ->save();
+        }
+
+        return back()
+            ->with(
+                'success',
+                __('contractors.bank_account_details.actions.update.success')
+            );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param BankAccountDetail $bankAccountDetail
+     *
+     * @return RedirectResponse
+     */
+    public function destroy(BankAccountDetail $bankAccountDetail)
+    {
+        $bankAccountDetail->delete();
+
+        return back()
+            ->with(
+                'success',
+                __(
+                    'contractors.bank_account_details.actions.destroy.success',
+                    ['name' => $bankAccountDetail->payment_account]
+                )
+            );
+    }
+
+    /**
+     * Restore the specified resource from storage.
+     *
+     * @param BankAccountDetail $bankAccountDetail
+     *
+     * @return RedirectResponse
+     */
+    public function restore(BankAccountDetail $bankAccountDetail)
+    {
+        $bankAccountDetail->restore();
+
+        return back()
+            ->with(
+                'success',
+                __(
+                    'contractors.bank_account_details.actions.restore.success',
+                    ['name' => $bankAccountDetail->payment_account]
+                )
+            );
+    }
+}
---
+++ routes/admin/organizations/bank_account_details.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Organizations\BankAccountDetailController as Controller;
+
+Route::resource('bank_account_details', Controller::class)
+    ->except(['create', 'edit', 'show', 'update', 'index']);
+Route::controller(Controller::class)->group(static function () {
+    Route::patch('/bank_account_details/update', 'update')
+        ->name('bank_account_details.update');
+    Route::post('/bank_account_details/{bank_account_detail}/restore', 'restore')
+        ->name('bank_account_details.restore')
+        ->withTrashed();
+});
