620f64f36e7a1568eb0f1fbb039c45c5c4613eb6
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-03-06 14:02:50 +0300

    update delete button

--- resources/views/components/buttons/delete.blade.php
+++ resources/views/components/buttons/delete.blade.php
@@ -4,8 +4,8 @@
        id="button-{{$itemId}}"
        onclick="event.preventDefault();document.getElementById('destroy-form-{{$itemId}}').submit();"
        title="{{__('datatable.buttons.delete')}}">
-        <i class="bi bi-trash3-fill"></i>
-        <form></form>
+        <i class="bi bi-trash3-fill align-middle"></i>
+        <form class="m-0"></form>
         <form id="destroy-form-{{$itemId}}"
               action="{{$route}}"
               method="POST"
