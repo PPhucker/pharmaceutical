2c9c4cd9abc7f449e90dec35deb64f1228d701b2
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-03-23 11:43:46 +0300

    update end-products.php routes

--- routes/classifiers/nomeclature/products/end-products.php
+++ routes/classifiers/nomeclature/products/end-products.php
@@ -10,14 +10,4 @@ Route::controller(Controller::class)
         Route::post('/end_products/{end_product}/restore', 'restore')
             ->name('end_products.restore')
             ->withTrashed();
-        Route::patch('/end_products/{end_product}/attach_material', 'attachMaterial')
-            ->name('end_products.attach_material');
-        Route::patch('/end_products/{end_product}/detach_material', 'detachMaterial')
-            ->name('end_products.detach_material');
-        Route::patch('/end_products/{end_product}/attach_aggregation_type', 'attachAggregationType')
-            ->name('end_products.attach_aggregation_type');
-        Route::patch('/end_products/{end_product}/detach_aggregation_type', 'detachAggregationType')
-            ->name('end_products.detach_aggregation_type');
-        Route::patch('/end_products/{end_product}/update_product_quantity', 'updateProductQuantity')
-            ->name('end_products.update_product_quantity');
     });
