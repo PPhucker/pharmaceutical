2e5e5cfc8fd0751931c742a74ec52233408af6f5
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-03-23 11:55:16 +0300

    update migrations

--- database/migrations/2023_03_07_105421_create_classifier_end_products_table.php
+++ database/migrations/2023_03_07_105421_create_classifier_end_products_table.php
@@ -59,12 +59,6 @@ class CreateClassifierEndProductsTable extends Migration
                 ->on('classifier_okpd2')
                 ->cascadeOnUpdate()
                 ->nullOnDelete();
-            /*$table->unsignedBigInteger('mdlp_id')
-                ->nullable()
-                ->comment('ID из mdlp.iz');*/
-            /*$table->char('GTIN', 14)
-                ->nullable()
-                ->comment('Global Trade Item Number');*/
             $table->string('short_name', 50)
                 ->comment('Краткое название');
             $table->string('full_name', 150)
