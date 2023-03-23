f3e35872a07993e0450931d6feb3890bc8eac9db
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-03-23 11:44:46 +0300

    update web.php
    added nomenclature routes

--- routes/classifiers/web.php
+++ routes/classifiers/web.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('classifiers')->group(static function () {
     require_once __DIR__ . '/legal-forms.php';
     require_once __DIR__ . '/banks.php';
+    require_once __DIR__. '/nomeclature/web.php';
 });
