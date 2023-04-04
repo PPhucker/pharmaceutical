2c62045fdf25ef72dfcac7e95d6311c437c92fb3
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-04-04 09:56:59 +0300

    update routes

--- routes/admin/users.php
+++ routes/admin/users.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\Admin\Users\UserController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users/register', [RegisterController::class, 'showRegistrationForm'])
-    ->name('users.register');
-
 Route::resource('users', UserController::class)
     ->except('show');
+
+Route::get('/users/register', [RegisterController::class, 'showRegistrationForm'])
+    ->name('users.register');
 
 Route::controller(UserController::class)->group(static function () {
     Route::post('/users/{user}/restore', 'restore')
