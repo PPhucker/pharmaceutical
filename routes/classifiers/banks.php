e09f9671d574e5a9cffb8d8781a6996963199830
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-04-06 14:40:49 +0300

    update routes

--- routes/classifiers/banks.php
+++ routes/classifiers/banks.php
@@ -4,7 +4,4 @@ use App\Http\Controllers\Classifiers\BankController as Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('banks', Controller::class)
-    ->only(['index', 'store']);
-
-Route::patch('banks/update', [Controller::class, 'update'])
-    ->name('banks.update');
+    ->only(['index', 'store', 'update']);
--- routes/classifiers/legal-forms.php
+++ routes/classifiers/legal-forms.php
@@ -4,7 +4,4 @@ use App\Http\Controllers\Classifiers\LegalFormController as Controller;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('legal_forms', Controller::class)
-    ->only(['index', 'store']);
-
-Route::patch('legal_forms/update', [Controller::class, 'update'])
-    ->name('legal_forms.update');
+    ->only(['index', 'store', 'update']);
