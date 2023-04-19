18524c7d0c785d5a45e0d133b57324fc9f674870
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-04-19 12:43:23 +0300

    update show.blade.php

--- resources/views/documents/invoices-for-payment/show.blade.php
+++ resources/views/documents/invoices-for-payment/show.blade.php
@@ -1,1 +1,14 @@
-<?php
+@extends('layouts.app')
+@section('content')
+    {{--{{dd($data)}}--}}
+    <x-forms.document back="{{route('invoices_for_payment.index')}}"
+                      title="
+                  {{__('documents.invoices_for_payment.invoice_for_payment')
+                    . ' №'
+                    . $invoiceForPayment->number
+                    . ' '
+                    . $invoiceForPayment->date}}
+                  ">
+        @include('templates.documents.invoice_for_payment')
+    </x-forms.document>
+@endsection
