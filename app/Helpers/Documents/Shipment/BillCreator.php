23652f9641e93ec93e97cb7d3f8c2c9dfceb70cc
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-05-03 14:17:08 +0300

    create BillCreator

---
+++ app/Helpers/Documents/Shipment/BillCreator.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Helpers\Documents\Shipment;
+
+use App\Helpers\Documents\Creator;
+use App\Models\Classifiers\Nomenclature\Products\EndProduct;
+use App\Models\Documents\Shipment\PackingLists\PackingListProduct;
+
+class BillCreator extends Creator
+{
+    protected $countProductsOnPages = [4, 6];
+
+    /**
+     * @return object
+     */
+    public function getData()
+    {
+        $productsOnPage = $this->getProductionOnPage();
+
+        $organization = $this->document->organization;
+        $contactor = $this->document->contractor;
+
+        return (object)[
+            'organization' => (object)[
+                'shipper' => $this->getShipperField(),
+                'name' => $this->getOrganizationFullName($organization),
+                'registered' => $this->getOrganizationRegisteredAddress($organization),
+                'inn' => $organization->INN,
+                'kpp' => $organization->kpp,
+            ],
+            'contractor' => (object)[
+                'consignee' => $this->getConsigneeField(),
+                'delivery_address' => $this->getDeliveryAddressField(),
+                'name' => $this->getContractorFullName($contactor),
+                'registered' => $this->getContractorRegisteredAddress($contactor),
+                'inn' => $contactor->INN,
+                'kpp' => $contactor->kpp,
+            ],
+            'packing_list' => (object)[
+                'number' => $this->document->number,
+                'date' => $this->document->date
+            ],
+            'pages' => $productsOnPage->pages,
+            'total' => $productsOnPage->total,
+            'count_production' => $this->getCountProductionField(),
+            'count_places' => $this->getCountPlacesField(),
+            'total_sum' => $this->getTotalSumField(),
+            'storekeeper' => $this->document->storekeeper,
+            'bookkeeper' => $this->document->bookkeeper,
+            'director' => $this->document->director,
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    protected function getShipperField()
+    {
+        $organization = $this->document->organization;
+
+        $placeOfBusiness = $this->document->organizationPlaceOfBusiness;
+
+        return $this->getOrganizationFullName($organization)
+            . ', '
+            . $placeOfBusiness->index
+            . ', '
+            . $placeOfBusiness->address;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getConsigneeField()
+    {
+        $contractor = $this->document->contractor;
+
+        $placeOfBusiness = $this->document->contractorPlaceOfBusiness;
+
+        return $this->getContractorFullName($contractor)
+            . ', '
+            . $placeOfBusiness->index
+            . ', '
+            . $placeOfBusiness->address;
+    }
+
+    /**
+     * @param EndProduct         $endProduct
+     * @param PackingListProduct $packingListProduct
+     *
+     * @return string
+     */
+    protected function getProductFullName(EndProduct $endProduct, PackingListProduct $packingListProduct)
+    {
+        return $endProduct->full_name
+            . ' '
+            . $endProduct->registrationNumber->number
+            . ' код ОКПД2 '
+            . $endProduct->okpd2->code
+            . ' '
+            . '(' . $packingListProduct->series . ')';
+    }
+}
