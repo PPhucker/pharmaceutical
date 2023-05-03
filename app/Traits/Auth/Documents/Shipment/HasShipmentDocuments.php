5aaf65346e8b4eaf623f505acd570129073748cf
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-05-03 14:09:25 +0300

    update HasShipmentDocuments

--- app/Traits/Auth/Documents/Shipment/HasShipmentDocuments.php
+++ app/Traits/Auth/Documents/Shipment/HasShipmentDocuments.php
@@ -2,6 +2,7 @@
 
 namespace App\Traits\Auth\Documents\Shipment;
 
+use App\Models\Documents\Shipment\Bills\Bill;
 use App\Models\Documents\Shipment\PackingLists\PackingList;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -29,5 +30,23 @@ trait HasShipmentDocuments
     public function approvedPackingLists()
     {
         return $this->hasMany(PackingList::class, 'approved_by_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function createdBills()
+    {
+        return $this->hasMany(Bill::class, 'created_by_id');
+    }
+
+    public function updatedBills()
+    {
+        return $this->hasMany(Bill::class, 'updated_by_id');
+    }
+
+    public function approvedBills()
+    {
+        return $this->hasMany(Bill::class, 'approved_by_id');
     }
 }
