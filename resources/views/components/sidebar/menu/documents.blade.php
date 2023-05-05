c7145e7a9c61d323ec8d32e63e9688bf879241de
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-05-05 10:08:22 +0300

    update sidebar

--- resources/views/components/sidebar/menu/documents.blade.php
+++ resources/views/components/sidebar/menu/documents.blade.php
@@ -43,6 +43,11 @@
                     <x-sidebar.menu.dropdown.link title="{{__('form.titles.list')}}"
                                                   route="{{route('bills.index')}}"/>
                 </x-sidebar.menu.dropdown.item>
+                <x-sidebar.menu.dropdown.item icon="bi bi-file-earmark-text"
+                                              title="{{__('documents.shipment.appendixes.appendixes')}}">
+                    <x-sidebar.menu.dropdown.link title="{{__('form.titles.list')}}"
+                                                  route="{{route('appendixes.index')}}"/>
+                </x-sidebar.menu.dropdown.item>
             </div>
 
         </div>
