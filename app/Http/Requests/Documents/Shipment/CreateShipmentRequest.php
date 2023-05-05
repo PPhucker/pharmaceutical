6d9cc65837a4ca01d0c5194571f89d1a04b6129e
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-05-05 09:47:14 +0300

    update CreateShipmentRequest

--- app/Http/Requests/Documents/Shipment/CreateShipmentRequest.php
+++ app/Http/Requests/Documents/Shipment/CreateShipmentRequest.php
@@ -3,9 +3,13 @@
 namespace App\Http\Requests\Documents\Shipment;
 
 use App\Http\Requests\CoreFormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Validator;
 
 abstract class CreateShipmentRequest extends CoreFormRequest
 {
+    protected $key = '';
+
     /**
      * @var array[]
      */
@@ -15,4 +19,58 @@ abstract class CreateShipmentRequest extends CoreFormRequest
             'numeric',
         ],
     ];
+
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'packing_list_id' => [
+                'required',
+                'numeric',
+                Rule::unique(
+                    'documents_shipment_' . $this->key,
+                    'packing_list_id'
+                )
+                    ->whereNull('deleted_at'),
+            ],
+        ];
+    }
+
+
+    /**
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'packing_list_id.unique' => __(
+                'documents.shipment.' . $this->key . '.errors.packing_list_id.unique'
+            ),
+        ];
+    }
+
+    /**
+     * @param Validator $validator
+     *
+     * @return void
+     */
+    public function withValidator(Validator $validator)
+    {
+        $validator->after(function ($validator) {
+            if ($validator->errors()->isNotEmpty()) {
+                foreach ($validator->errors()->get('packing_list_id') as $message) {
+                    $validator->errors()->add(
+                        'alert-errors',
+                        $message
+                    );
+                }
+                $validator->errors()->add(
+                    'fail',
+                    __($this->afterValidatorFailKeyMessage)
+                );
+            }
+        });
+    }
 }
