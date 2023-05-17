ca09b00498c17d986e69406bd6c95b36324e4d35
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-05-17 11:00:12 +0300

    create Waybill form requests

---
+++ app/Http/Requests/Documents/Shipment/Waybills/IndexWaybillRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Requests\Documents\Shipment\Waybills;
+
+use App\Http\Requests\Documents\Shipment\IndexShipmentRequest;
+
+class IndexWaybillRequest extends IndexShipmentRequest
+{
+}
