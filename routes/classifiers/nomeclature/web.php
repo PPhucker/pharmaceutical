f7577b5869888f8520fd5cbf4d34e3192719e575
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-05-29 10:39:10 +0300

    update routes

--- routes/classifiers/nomeclature/web.php
+++ routes/classifiers/nomeclature/web.php
@@ -6,4 +6,5 @@ Route::prefix('nomenclature')->group(static function () {
     require_once __DIR__ . '/products/web.php';
     require_once __DIR__ . '/materials/web.php';
     require_once __DIR__ . '/okei.php';
+    require_once __DIR__ . '/services/web.php';
 });
