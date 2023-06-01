e76e4ebbee27564e87e8846e37fabc15738b82be
PPhucker <alexandr.priponov.pphucker@gmail.com>
2023-06-01 10:19:47 +0300

    update routes

--- routes/classifiers/nomeclature/materials/web.php
+++ routes/classifiers/nomeclature/materials/web.php
@@ -7,8 +7,7 @@ Route::prefix('materials')->group(static function () {
     require_once __DIR__ . '/types_of_materials.php';
 });
 
-Route::resource('materials', Controller::class)
-    ->except(['show', 'edit', 'create']);
+Route::resource('materials', Controller::class);
 Route::controller(Controller::class)->group(static function () {
     Route::post('/materials/{material}/restore', 'restore')
         ->name('materials.restore')
