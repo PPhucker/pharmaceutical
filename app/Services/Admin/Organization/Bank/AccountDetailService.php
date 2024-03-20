d41e1d2843a71f144f51ebe88b3db3de585869c6
PPhucker <alexandr.priponov.pphucker@gmail.com>
2024-03-20 10:07:58 +0300

    update AccountDetailService

--- app/Services/Admin/Organization/Bank/AccountDetailService.php
+++ app/Services/Admin/Organization/Bank/AccountDetailService.php
@@ -23,5 +23,7 @@ class AccountDetailService extends ContractorAccountDetailService
 
         $this->repositories->accountDetail = app()
             ->make(BankAccountDetailRepository::class);
+
+        $this->selectedRepo = $this->repositories->accountDetail;
     }
 }
