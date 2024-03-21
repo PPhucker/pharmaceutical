134347804d55cc4ec0fda25bc3b12e57ae1230ae
PPhucker <alexandr.priponov.pphucker@gmail.com>
2024-03-21 10:36:18 +0300

    update blade components

--- resources/views/components/form/element/input.blade.php
+++ resources/views/components/form/element/input.blade.php
@@ -1,7 +1,8 @@
+@aware(['model'])
 <input id="{{$id}}"
        type="{{$type}}"
        class="{{$class}} col-auto m-1 @if($type !== 'radio' && $type !== 'checkbox') border-0 @endif text-primary @error($errorName) is-invalid @enderror"
-       name="{{$name}}"
+       name="@if($model && $model->trashed())trashed-@endif{{$name}}"
        value="{{$value}}"
        @if($required) required @endif
        @if($readonly) readonly @endif
